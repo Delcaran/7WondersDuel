fe30a099617167b09914c58aeb6c27fa981216fd
Matteo Paoluzzi <delcaran@vbufficio>
2021-02-22 11:26:23 +0100

    Stampa delle carte e del pannello di destra, per ora vuoto

--- gui/gui.go
+++ gui/gui.go
@@ -8,14 +8,19 @@ import (
 )
 
 func createBoardTable(game *game.Game) *tview.Table {
-	var title string
-	fmt.Sprintf(title, "Age %d", game.CurrentAge)
-	boardTable := tview.NewTable().SetBorders(false)
-	boardTable.Box.SetTitle(title)
+	boardTable := tview.NewTable().SetBorders(false).SetSelectable(true, true)
+	minX := 10000
+	maxY := 0
 	for r := 0; r <= game.Board.YMax; r++ {
 		for c := 0; c < game.Board.XMax; c++ {
 			card := game.Board.Cards[r][c]
 			if card.Building != nil {
+				if r > maxY {
+					maxY = r
+				}
+				if c < minX {
+					minX = c
+				}
 				cardName := "?"
 				color := tcell.ColorWhite
 				if card.Visible {
@@ -24,11 +29,15 @@ func createBoardTable(game *game.Game) *tview.Table {
 				if game.Board.CardBlocked(&card) {
 					color = tcell.ColorYellow
 				}
-				cell := tview.NewTableCell(cardName).SetTextColor(color).SetAlign(tview.AlignCenter)
+				cell := tview.NewTableCell(cardName).
+					SetTextColor(color).
+					SetAlign(tview.AlignCenter)
 				boardTable = boardTable.SetCell(r, c, cell)
 			}
 		}
 	}
+	boardTable.Select(maxY, minX) // first "buildable" card to the left
+
 	return boardTable
 }
 
@@ -37,9 +46,15 @@ func Gui(game *game.Game) *tview.Application {
 	game.CurrentAge = 1
 	game.DeployBoard()
 
-	// GUI done, it's time to play
-	boardTable := createBoardTable(game)
-	app := tview.NewApplication().SetRoot(boardTable, true)
+	title := fmt.Sprintf("7 Wonders Duel - Age %d", game.CurrentAge)
 
+	boardTable := createBoardTable(game)
+	infoFrame := tview.NewFrame(nil).AddText(title, true, tview.AlignCenter, tcell.ColorGreen)
+	mainGrid := tview.NewGrid().SetBorders(true)
+	mainGrid.AddItem(boardTable, 0, 0, 1, 1, 0, 0, true)
+	mainGrid.AddItem(infoFrame, 0, 1, 1, 1, 0, 0, false)
+	app := tview.NewApplication().SetRoot(mainGrid, true)
+
+	// GUI done, it's time to play
 	return app
 }

e898d8ae951d1eb0701b2dd23f867bb98450db84
Matteo Paoluzzi <delcaran@vmufficio.com>
2022-03-30 13:53:53 +0200

    Added Fyne for real GUI - TBD

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
-import "7WondersDuel/game"
-import "7WondersDuel/gui"
+import (
+	"os"
+
+	"7WondersDuel/game"
+	"7WondersDuel/gui"
+	"7WondersDuel/tui"
+)
 
 func main() {
 	var match game.Game
-	app := gui.Gui(&match)
-	if err := app.Run(); err != nil {
-		panic(err)
+
+	useTui := len(os.Args[1:]) >= 1 && os.Args[1] == "tui"
+
+	if useTui {
+		app := tui.Tui(&match)
+		if err := app.Run(); err != nil {
+			panic(err)
+		}
+	} else {
+		app := gui.Gui()
+		(*app).Run()
 	}
 }
